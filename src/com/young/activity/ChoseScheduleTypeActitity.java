f98b7fb3314ebf6d5e7d5b598c1401d15122dfb3
veryyoung <codingyoung@gmail.com>
2014-03-16 22:45:42 +0800

    table host of schedule is ok

--- src/com/young/activity/ChoseScheduleTypeActitity.java
+++ src/com/young/activity/ChoseScheduleTypeActitity.java
@@ -1,46 +1,60 @@
 package com.young.activity;
 
+import android.app.TabActivity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.ImageView;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.RadioGroup.OnCheckedChangeListener;
+import android.widget.TabHost;
 
 import com.young.R;
 
 /**
  * Created by VERYYOUNG on 14-3-8.
  */
-public class ChoseScheduleTypeActitity extends BaseActivity {
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_chose_schedule_type);
-        //网络课表
-        ImageView netScheduleView = (ImageView) findViewById(R.id.net_schedule);
-        netScheduleView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(ChoseScheduleTypeActitity.this, ScheduleActivity.class);
-                startActivity(intent);
-            }
-        });
-        //小伙伴课表
-        ImageView othersScheduleView = (ImageView) findViewById(R.id.others_schedule);
-        othersScheduleView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(ChoseScheduleTypeActitity.this,InputOtherStuIdForScheduleActivity.class);
-                startActivity(intent);
-            }
-        });
-        //本地课表
-        ImageView localScheduleView = (ImageView) findViewById(R.id.local_schedule);
-        localScheduleView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(ChoseScheduleTypeActitity.this,LocalScheduleActivity.class);
-                startActivity(intent);
-            }
-        });
-    }
+@SuppressWarnings("deprecation")
+public class ChoseScheduleTypeActitity extends TabActivity implements
+		OnCheckedChangeListener {
+
+	private TabHost mHost;
+	private RadioGroup radioderGroup;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.tabhost_schedule);
+		mHost = this.getTabHost();
+		mHost.addTab(mHost.newTabSpec("ONE").setIndicator("ONE")
+				.setContent(new Intent(this, LocalScheduleActivity.class)));
+		mHost.addTab(mHost.newTabSpec("TWO").setIndicator("TWO")
+				.setContent(new Intent(this, ScheduleActivity.class)));
+		mHost.addTab(mHost
+				.newTabSpec("THREE")
+				.setIndicator("THREE")
+				.setContent(
+						new Intent(this,
+								InputOtherStuIdForScheduleActivity.class)));
+		radioderGroup = (RadioGroup) findViewById(R.id.main_radio);
+		radioderGroup.setOnCheckedChangeListener(this);
+		RadioButton radioButton = (RadioButton) findViewById(R.id.local_radio_button);
+		radioButton.setChecked(true);
+	}
+
+	@Override
+	public void onCheckedChanged(RadioGroup group, int checkedId) {
+		switch (checkedId) {
+		case R.id.local_radio_button:
+			mHost.setCurrentTabByTag("ONE");
+			break;
+		case R.id.net_radio_button:
+			mHost.setCurrentTabByTag("TWO");
+			break;
+		case R.id.others_radio_button:
+			mHost.setCurrentTabByTag("THREE");
+			break;
+		default:
+			break;
+		}
+	}
 }
