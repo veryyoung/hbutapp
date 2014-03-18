fd9fc489a59dccdb54086475849de4b4b118384c
Shedon Young <codingyoung@gmail.com>
2014-03-18 13:52:35 +0800

    delete log info

--- src/com/young/activity/AboutUsActivity.java
+++ src/com/young/activity/AboutUsActivity.java
@@ -1,15 +1,16 @@
 package com.young.activity;
 
 import android.os.Bundle;
+
 import com.young.R;
 
 public class AboutUsActivity extends BaseActivity {
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_about_us);
-	}
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about_us);
+    }
 
 
 }
