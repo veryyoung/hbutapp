7f2e9bf947d811a67833b1a825cd4864451cd37e
ironmanTony <iornmanlitao@gmail.com>
2014-03-18 13:04:00 +0800

    adjust score

---
+++ src/com/young/entry/ScoreInfo.java
@@ -0,0 +1,45 @@
+package com.young.entry;
+
+public class ScoreInfo {
+	private String stuId; //学号
+	private String averageGradePoint; // 总绩点
+	private String totalGradePoint;  // 平均绩点
+	private String name;  //姓名
+	private String content; // 内容，包括班级，学号等
+	public String getStuId() {
+		return stuId;
+	}
+	public void setStuId(String stuId) {
+		this.stuId = stuId;
+	}
+	
+	public String getAverageGradePoint() {
+		return averageGradePoint;
+	}
+	public void setAverageGradePoint(String averageGradePoint) {
+		this.averageGradePoint = averageGradePoint;
+	}
+	public String getTotalGradePoint() {
+		return totalGradePoint;
+	}
+	public void setTotalGradePoint(String totalGradePoint) {
+		this.totalGradePoint = totalGradePoint;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	
+	
+	
+	
+
+}
